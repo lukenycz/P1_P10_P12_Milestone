fed9eb62584fc2504e6f944d77e46440fb2701e3
Łukasz Nycz <lnycz96@gmail.com>
2021-07-29 19:58:15 +0200

    done.

---
+++ P1_P10_P12_Milestone/Photos.swift
@@ -0,0 +1,18 @@
+//
+//  Photos.swift
+//  P1_P10_P12_Milestone
+//
+//  Created by Łukasz Nycz on 29/07/2021.
+//
+
+import Foundation
+
+class Photos {
+    var name:String
+    var image: String
+    
+    init(name:String, image:String) {
+        self.name = name
+        self.image = image
+    }
+}
